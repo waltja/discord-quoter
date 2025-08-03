233a94da1591399d482e1e1575af5878d7714ff9
James <jtooley06@gmail.com>
2025-08-03 18:16:48 -0500

    Complete single user support, implemented full STT model based around 40s intervals

--- recording.py
+++ recording.py
@@ -1,10 +1,9 @@
 import asyncio
 import wave
 import discord
-from discord.ext import commands, voice_recv
 import torch
+from discord.ext import commands, voice_recv
 from transformers import AutoProcessor, AutoModelForSpeechSeq2Seq, pipeline
-
 
 # — Whisper Setup —
 MODEL_ID = "openai/whisper-large-v3"
@@ -15,62 +14,73 @@ processor = AutoProcessor.from_pretrained(MODEL_ID)
 print('Before Pipeline')
 pipe = pipeline(
     "automatic-speech-recognition",
-    model=AutoModelForSpeechSeq2Seq.from_pretrained(MODEL_ID, torch_dtype=dtype, low_cpu_mem_usage=True, use_safetensors=True).to(device),
+    model=AutoModelForSpeechSeq2Seq.from_pretrained(MODEL_ID, torch_dtype=dtype, low_cpu_mem_usage=False, use_safetensors=True).to(device),
     tokenizer=processor.tokenizer,
     feature_extractor=processor.feature_extractor,
     return_timestamps=True,
+    batch_size=4,
     device=device,
     torch_dtype=dtype,
     generate_kwargs={"language": "english", "task": "transcribe"}
 )
 
-
-def pcm_to_wav(pcm_data, wavfile = 'sample', bitrate = 48000):
+WAV_PATH = 'sample'
+def pcm_to_wav(pcm_data, wavfile = WAV_PATH, bitrate = 48000):
     with wave.open(wavfile+'.wav', 'wb') as wavefile:
         wavefile.setparams((1, 2, bitrate, 0, 'NONE', 'NONE'))
         wavefile.writeframes(pcm_data)
 
-async def stt(audiofile):
-    return pipe(audiofile)
-
 # - discord.quoter Setup -
+# noinspection SpellCheckingInspection
 TOKEN = 'MTM3MDYwMjk5Mjc3NjI1MzUzMQ.G6W0RR.avQa1j_tnylZfNa6kCcM8R2Q4t0AXau445ribg'  # Replace with your bot token
-GUILD_ID = 123456789012345678  # Replace with your server's ID
+SERVER_ID = 1102726131779633154  # Replace with your server's ID
 intents = discord.Intents.default()
 intents.message_content = True
 intents.voice_states = True
 
+# noinspection PyProtectedMember
 discord.opus._load_default()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 class Wav(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.pcm_data: bytes = b''
+        self.running = False
+        self.pcm_int = b''
+        self.data_int: list[bytes] = []
+        self.transcript = {}
 
     @commands.command()
     async def record(self, ctx):
+        users = []
         def callback(user, data: voice_recv.VoiceData):
-            print(f"Got packet from {user}")
-            self.pcm_data += data.pcm
+            if user not in users:
+                print(f"Got first packet from {user}")
+                users.append(user)
+            self.pcm_int += data.pcm
+
         if ctx.author.voice is None:
             await ctx.send("Please join a voice channel first.")
             return
-        try:
-            vc = await ctx.author.voice.channel.connect(cls=voice_recv.VoiceRecvClient, self_mute=True, reconnect=False)
-            vc.listen(voice_recv.BasicSink(callback))
-            while True:
-                await asyncio.sleep(5)
-                pcm_to_wav(self.pcm_data, bitrate=ctx.voice_client.channel.bitrate, wavfile='temp')
-                asyncio.run(stt, )
-        except discord.errors.ConnectionClosed as e:
-            print("Error: "+ str(e))
-            return
+
+        vc = await ctx.author.voice.channel.connect(cls=voice_recv.VoiceRecvClient, self_mute=True, reconnect=False)
+        vc.listen(voice_recv.BasicSink(callback))
+        self.running = True
+        while self.running:
+            await asyncio.sleep(10)
+            if len(self.data_int) >= 4:
+                self.data_int = self.data_int[1:]
+            self.data_int.append(self.pcm_int)
+            self.pcm_int = b''.join(self.data_int)
+            pcm_to_wav(self.pcm_int, bitrate=ctx.voice_client.channel.bitrate)
+            self.pcm_int = b''
+            self.transcript = pipe(WAV_PATH + '.wav')
+            print(self.transcript['text'])
+            print(self.transcript['chunks'])
 
     @commands.command()
     async def stop(self, ctx):
-        pcm_to_wav(self.pcm_data, bitrate=ctx.voice_client.channel.bitrate)
-        self.pcm_data = b''
+        self.running = False
         await ctx.voice_client.disconnect()
 
     @commands.command()
@@ -79,13 +89,6 @@ class Wav(commands.Cog):
         print(str(ctx.author) + " killed me")
         await ctx.bot.close()
 
-class Manager(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
-    @commands.command()
-    async def start(self, ctx):
-        pass
 
 @bot.event
 async def on_ready():
