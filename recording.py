ef6caed0c8a306ae5e0f9ab3c75f49b4c9529255
waltja <jtooley06@gmail.com>
2025-08-03 10:53:39 -0500

    Add full depth audio capture, implement initial STT model

--- recording.py
+++ recording.py
@@ -1,52 +1,85 @@
+import wave
 import discord
 from discord.ext import commands, voice_recv
-import asyncio
-import datetime
-import os
+import torch
+from transformers import AutoProcessor, AutoModelForSpeechSeq2Seq, pipeline
 
+
+# — Whisper Setup —
+# MODEL_ID = "openai/whisper-large-v3"
+# print(torch.cuda.is_available())
+# device    = "cuda" if torch.cuda.is_available() else "cpu"
+# dtype     = torch.float16 if device == "cuda" else torch.float32
+# processor = AutoProcessor.from_pretrained(MODEL_ID)
+# print('Before Pipeline')
+# pipe = pipeline(
+#     "automatic-speech-recognition",
+#     model=AutoModelForSpeechSeq2Seq.from_pretrained(MODEL_ID, torch_dtype=dtype, low_cpu_mem_usage=False, use_safetensors=True).to(device),
+#     tokenizer=processor.tokenizer,
+#     feature_extractor=processor.feature_extractor,
+#     return_timestamps=False,
+#     stride_length_s=(5, 5),
+#     device=device,
+#     torch_dtype=dtype,
+#     generate_kwargs={"language": "english", "task": "transcribe"}
+# )
+
+
+def pcm_to_wav(pcm_data, wavfile = 'sample.wav', bitrate = 44100):
+    with wave.open(wavfile, 'wb') as wavefile:
+        wavefile.setparams((1, 2, bitrate, 0, 'NONE', 'NONE'))
+        wavefile.writeframes(pcm_data)
+
+# - discord.quoter Setup -
 TOKEN = 'MTM3MDYwMjk5Mjc3NjI1MzUzMQ.G6W0RR.avQa1j_tnylZfNa6kCcM8R2Q4t0AXau445ribg'  # Replace with your bot token
 GUILD_ID = 123456789012345678  # Replace with your server's ID
-
-
 intents = discord.Intents.default()
 intents.message_content = True
 intents.voice_states = True
 
 discord.opus._load_default()
-bot = commands.Bot(command_prefix=commands.when_mentioned, intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 class Wav(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.pcm_data: bytes = b''
 
     @commands.command()
     async def record(self, ctx):
-
         def callback(user, data: voice_recv.VoiceData):
-            print(f"Got packet from {user}")
-            print(data)
-
+            print(f"Got packet from {user}:")
+            print(data.packet.payload)
+            print(data.packet.data)
+            self.pcm_data += data.pcm
         if ctx.author.voice is None:
             await ctx.send("You need to be in a voice channel to use this command.")
             return
-
-        vc = await ctx.author.voice.channel.connect(cls=voice_recv.VoiceRecvClient)
-        vc.listen(voice_recv.BasicSink(callback))
+        try:
+            vc = await ctx.author.voice.channel.connect(cls=voice_recv.VoiceRecvClient, self_mute=True, reconnect=False)
+            vc.listen(voice_recv.BasicSink(callback))
+        except discord.errors.ConnectionClosed as e:
+            print("Error: "+ str(e))
+            return
 
     @commands.command()
     async def stop(self, ctx):
-        await  ctx.voice_client.disconnect()
+        print(ctx.voice_client.channel.bitrate)
+        pcm_to_wav(self.pcm_data, bitrate=ctx.voice_client.channel.bitrate)
+        self.pcm_data = b''
+        await ctx.voice_client.disconnect()
 
     @commands.command()
-    async def die(self, ctx):
-        ctx.voice_client.stop()
+    async def kill(self, ctx):
+        await ctx.send("Dying -----")
+        print(str(ctx.author) + " killed me")
         await ctx.bot.close()
 
 class Manager(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command
+    @commands.command()
     async def start(self, ctx):
         pass
 
